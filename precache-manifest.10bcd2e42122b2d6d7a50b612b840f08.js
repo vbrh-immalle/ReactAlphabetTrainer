289ecb56ff59b75b2cbfe555796384599bb6851b
hansvb <hans.vanbroeckhoven@gmail.com>
2019-08-12 00:16:16 +0200

    Updates

---
+++ precache-manifest.10bcd2e42122b2d6d7a50b612b840f08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96273de7a194fb2fd2ae1a6174b8a1e",
+    "url": "/ReactAlphabetTrainer/index.html"
+  },
+  {
+    "revision": "24593e527dbacc219bd2",
+    "url": "/ReactAlphabetTrainer/static/css/main.4249fb2b.chunk.css"
+  },
+  {
+    "revision": "1d91af053e5786c50510",
+    "url": "/ReactAlphabetTrainer/static/js/2.8a789ac4.chunk.js"
+  },
+  {
+    "revision": "24593e527dbacc219bd2",
+    "url": "/ReactAlphabetTrainer/static/js/main.89bd3de7.chunk.js"
+  },
+  {
+    "revision": "c35624be522dc7ab83d6",
+    "url": "/ReactAlphabetTrainer/static/js/runtime~main.0d56bd65.js"
+  }
+]);
